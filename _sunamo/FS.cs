b27dc3e7cff682556c234b9913bd6277c09edac1
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 19:04:18 +0200

    Before create public all except of _sunamo

--- _sunamo/FS.cs
+++ _sunamo/FS.cs
@@ -1,4 +1,4 @@
-//namespace SunamoCollectionOnDrive._sunamo.SunamoExceptions._AddedToAllCsproj;
+namespace SunamoCollectionOnDrive._sunamo;
 
 //namespace SunamoCollectionOnDrive._sunamo.SunamoExceptions._AddedToAllCsproj;
 internal class FS
