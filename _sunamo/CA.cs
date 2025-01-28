b02b245d5516ab560812d1a30c8f66e594084e5f
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-28 09:44:30 +0100

    Buildable, removed unused internal methods in _sunamo

--- _sunamo/CA.cs
+++ _sunamo/CA.cs
@@ -2,25 +2,7 @@ namespace SunamoCollectionOnDrive._sunamo;
 
 internal class CA
 {
-    internal static List<string> RemoveStringsEmptyTrimBefore(List<string> mySites)
-    {
-        for (var i = mySites.Count - 1; i >= 0; i--)
-            if (mySites[i].Trim() == string.Empty)
-                mySites.RemoveAt(i);
-        return mySites;
-    }
 
-    internal static bool IsListStringWrappedInArray<T>(List<T> v2)
-    {
-        var first = v2.First().ToString();
-        if (v2.Count == 1 && (first == "System.Collections.Generic.List`1[System.String]" ||
-        first == "System.Collections.Generic.List`1[System.Object]")) return true;
-        return false;
-    }
-    internal static void InitFillWith(List<string> datas, int pocet, string initWith = "")
-    {
-        InitFillWith<string>(datas, pocet, initWith);
-    }
     internal static void InitFillWith<T>(List<T> datas, int pocet, T initWith)
     {
         for (int i = 0; i < pocet; i++)
