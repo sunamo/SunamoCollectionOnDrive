b419637e4a28fe8abd63b33a39d8785222229aef
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 20:56:58 +0100

    25.1.29.1 Deleted private, protected, internal unused methods

--- _sunamo/CA.cs
+++ _sunamo/CA.cs
@@ -3,11 +3,4 @@ namespace SunamoCollectionOnDrive._sunamo;
 internal class CA
 {
 
-    internal static void InitFillWith<T>(List<T> datas, int pocet, T initWith)
-    {
-        for (int i = 0; i < pocet; i++)
-        {
-            datas.Add(initWith);
-        }
-    }
 }
